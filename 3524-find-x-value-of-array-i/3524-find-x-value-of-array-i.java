42a5b364fb540c694d568dc560c0fc99027163e1
DARSH SOAM <darsh.soam.cseds.2024@miet.ac.in>
2026-09-21 21:59:47 +0530

    Time: 12 ms (92.31%), Space: 93.6 MB (38.46%) - LeetHub

---
+++ 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.java
@@ -0,0 +1,25 @@
+class Solution {
+    public long[] resultArray(int[] nums, int k) {
+        long[] res = new long[k];
+        long[] previousRemainderCount = new long[k];
+
+        for(int num : nums) {
+            long[] currentRemainderCount = new long[k];
+
+            int numRemainder = num % k;
+            currentRemainderCount[numRemainder]++;
+
+            for(int oldRemainder = 0; oldRemainder < k; oldRemainder++){
+                int newRemainder = (oldRemainder * numRemainder) % k;
+                currentRemainderCount[newRemainder] += previousRemainderCount[oldRemainder];
+            }
+
+            previousRemainderCount = currentRemainderCount;
+
+            for(int remainder = 0; remainder < k; remainder++) {
+                res[remainder] += previousRemainderCount[remainder];
+            }
+        }
+        return res;
+    }
+}
