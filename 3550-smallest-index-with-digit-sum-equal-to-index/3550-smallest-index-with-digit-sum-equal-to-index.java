849db0f29d15433edb9d7abbe65153249d617611
DARSH SOAM <darsh.soam.cseds.2024@miet.ac.in>
2026-09-24 22:44:02 +0530

    Time: 1 ms (99.69%), Space: 45.6 MB (40.74%) - LeetHub

---
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int smallestIndex(int[] nums) {
+        for(int i = 0; i < nums.length; i++) {
+            int val = nums[i];
+            int sum = 0;
+            while(val > 0) {
+                int lastCh = val % 10;
+                sum += lastCh;
+                val /= 10;
+            }
+            if(sum == i) return i;
+        }
+        return -1;
+    }
+}
