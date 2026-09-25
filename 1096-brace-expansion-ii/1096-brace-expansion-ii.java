29ffe5c85f02b985eaf93b85fe5620ae45499233
DARSH SOAM <darsh.soam.cseds.2024@miet.ac.in>
2026-09-25 22:22:59 +0530

    Time: 16 ms (36.8%), Space: 53.1 MB (6.4%) - LeetHub

--- 1096-brace-expansion-ii/1096-brace-expansion-ii.java
+++ 1096-brace-expansion-ii/1096-brace-expansion-ii.java
@@ -2,31 +2,32 @@ import java.util.*;
 
 class Solution {
     public List<String> braceExpansionII(String expression) {
-        Set<String> result = parse(expression,0,expression.length());
+        Set<String> result = parse(expression, 0, expression.length());
         List<String> ans = new ArrayList<>(result);
         Collections.sort(ans);
         return ans;
     }
-    private Set<String> parse(String s,int start, int end){
+
+    private Set<String> parse(String s, int start, int end) {
         Set<String> result = new HashSet<>();
         Set<String> current = new HashSet<>();
         current.add("");
 
         int i = start;
-        while(i<end){
+        while (i < end) {
             char ch = s.charAt(i);
-            if(ch == ','){
+            if (ch == ',') { 
                 result.addAll(current);
                 current = new HashSet<>();
                 current.add("");
                 i++;
-            } else if(ch == '{'){
+            } else if (ch == '{') {
                 int balance = 1;
                 int j = i + 1;
-                while(j < end && balance > 0){
-                    if(s.charAt(j) == '{') {
+                while (j < end && balance > 0) {
+                    if (s.charAt(j) == '{') {
                         balance++;
-                    } else if(s.charAt(j) == '}'){
+                    } else if (s.charAt(j) == '}') {
                         balance--;
                     }
                     j++;
@@ -37,23 +38,24 @@ class Solution {
                 i = j;
             } else {
                 Set<String> temp = new HashSet<>();
-                for(String str : current){
-                    temp.add(str+ch);
+                for (String str : current) {
+                    temp.add(str + ch);
                 }
                 current = temp;
                 i++;
-                }
             }
-            result.addAll(current);
-            return result;
-        }   
-        public Set<String> multiply(Set<String> a,Set<String> b){
-            Set<String> result = new HashSet<>();
-            for(String x:a){
-                for(String y:b){
-                    result.add(x+y);
-                }
-            }
-            return result;
         }
+        result.addAll(current);
+        return result;
+    }   
+
+    public Set<String> multiply(Set<String> a, Set<String> b) {
+        Set<String> result = new HashSet<>();
+        for (String x : a) {
+            for (String y : b) {
+                result.add(x + y);
+            }
+        }
+        return result;
     }
+} 
